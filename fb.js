d306b7cd75354d3dc2b52fcede881de3cf36a75e
Jason Lee <jason@172-18-20-33.DYNAPOOL.NYU.EDU>
2017-04-27 21:43:19 -0400

    fixed facebook callback url

--- fb.js
+++ fb.js
@@ -1,5 +1,5 @@
 module.exports = {
   'appID' : '303522516749628',
   'appSecret' : '92dc51c76647615de3276ce596643994',
-  'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
+  'callbackUrl' : '/login/facebook/callback'
 }
